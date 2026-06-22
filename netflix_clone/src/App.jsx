545b519cc06f22c29805239d1bbea2356246ceb9
humblyy <humblyy297@gmail.com>
2026-06-22 15:13:31 -0400

    axios, request and TMDB API  is constructed

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -1,6 +1,20 @@
 import Home from './pages/home/Home'
 import './App.css'
+//tmdb api key 
+//now playing movies 
+/*const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjUyYjY5MDg1NWM2NWYyNmRiN2Q1MGM5MzViYzMwYyIsIm5iZiI6MTc4MjEzNjE0NS45NzEsInN1YiI6IjZhMzkzZDUxYjQ5NWZkMWYwMTQ1NDIwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2ykGxVMyK6-BbyJVfKoGmUjqW8SRhywD9GPhxHXlalM'
+  }
+};
 
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+*/
 function App() {
 
 
---
+++ netflix_clone/src/utils/axios.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios, { create } from "axios"
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+function axios() {
+  return (
+    <div>
+        
+
+
+    </div>
+  )
+}
+
+export default axios
